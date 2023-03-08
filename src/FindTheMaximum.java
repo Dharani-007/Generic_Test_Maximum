c514d213ebf8e19734a40fd79aa0deae504abe1d
Dharani 007 <dharanimeenatchisundharam112@gmail.com>
2023-03-09 00:20:23 +0530

    Added : Given 3 Integers find the maximum

--- src/FindTheMaximum.java
+++ src/FindTheMaximum.java
@@ -1,3 +1,18 @@
-public class FineTheMaximum {
-    
+public class FindTheMaximum<T extends Comparable<T>> {
+    public <T extends Comparable<T>> T findMaximum(T x, T y, T z) {
+        T max = x;
+
+        if (y.compareTo(max) > 0)
+            max = y;
+
+        if (z.compareTo(max) > 0)
+            max = z;
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        FindTheMaximum genericsFindMax = new FindTheMaximum();
+        genericsFindMax.findMaximum(6, 7, 5);
+    }
 }
