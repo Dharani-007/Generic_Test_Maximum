8b05fedacaeff528603a385ce081a89285785726
Dharani 007 <dharanimeenatchisundharam112@gmail.com>
2023-03-15 21:21:32 +0530

    Added : Extend the max  method to take more then three parameters

--- src/FindTheMaximum.java
+++ src/FindTheMaximum.java
@@ -1,16 +1,26 @@
-public class FindTheMaximum {
-    public static String maxOfString(String s1, String s2, String s3) {
-        // Method used to compare three String value and get maximum value
-        String max = s1;
-        if (s2.compareTo(max) > 0)
-            max = s2;
-        if (s3.compareTo(max) > 0)
-            max = s3;
+public class FindTheMaximum<T extends Comparable <T>> {
+    T a1, a2, a3, a4;
+
+    public FindTheMaximum(T a1, T a2, T a3, T a4) {
+        this.a1 = a1;
+        this.a2 = a2;
+        this.a3 = a3;
+        this.a4 = a4;
+    }
+    public static <T extends Comparable <T>> T maxOfValues(T a1, T a2, T a3, T a4) {
+        T max = a1;
+        if (a2.compareTo(max) > 0)
+            max = a2;
+        if (a3.compareTo(max) > 0)
+            max = a3;
+        if (a4.compareTo(max) > 0)
+            max = a4;
         return max;
     }
     public static void main(String[] args) {
-        System.out.println("Welcome to the find maximum value of three Integer using without Generics\n");
-        System.out.println("The maximum value between the three integer is : " + maxOfString ("abc", "pqr", "xyz"));
+        System.out.println("Welcome to the find maximum value of three Integer using Class\n");
+        System.out.println("The maximum value between the three Integer is : " + maxOfValues (2, 25, 50, 60)+ "\n");
+        System.out.println("The maximum value between the three Float is : " + maxOfValues (2.7f, 25.5f, 50.9f, 44.9f)+ "\n");
+        System.out.println("The maximum value between the three String is : " + maxOfValues ("abc", "pug", "zca", "zza"));
     }
-
 }
