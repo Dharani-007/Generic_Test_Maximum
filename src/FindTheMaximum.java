89bf1364f44d83799d99a860971d11840e5f5fe2
Dharani 007 <dharanimeenatchisundharam112@gmail.com>
2023-03-15 21:06:58 +0530

    Added : Given 3 Strings find the maximum

--- src/FindTheMaximum.java
+++ src/FindTheMaximum.java
@@ -1,16 +1,16 @@
-public class FindTheMaximum {
-    // Method used to compare three Float value and get maximum value
-    public static Float maxOfFloat(Float f1, Float f2, Float f3) {
-        Float max = f1;
-        if (f2.compareTo(max) > 0)
-            max = f2;
-        if (f3.compareTo(max) > 0)
-            max = f3;
+public class StringMaxValue {
+    public static String maxOfString(String s1, String s2, String s3) {
+        // Method used to compare three String value and get maximum value
+        String max = s1;
+        if (s2.compareTo(max) > 0)
+            max = s2;
+        if (s3.compareTo(max) > 0)
+            max = s3;
         return max;
     }
     public static void main(String[] args) {
         System.out.println("Welcome to the find maximum value of three Integer using without Generics\n");
-        System.out.println("The maximum value between the three integer is : " + maxOfFloat (2.5f, 25.7f, 50.9f));
+        System.out.println("The maximum value between the three integer is : " + maxOfString ("abc", "pqr", "xyz"));
     }
 
 }
